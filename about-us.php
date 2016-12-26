8410f0cb242991b6651c4ff9069d4db50aa36042
Elizabeth Mateas <elizabethmateas@Elizabeths-MacBook.local>
2016-12-26 20:33:27 +0200

    added content in about-us page

--- about-us.php
+++ about-us.php
@@ -1,5 +1,21 @@
 <?php include 'header.php'; ?>
-  <div id="main" class="">
-      
+  <div id="main" class="about-us-page inner">
+      <div class="section1 clearfix">
+        <div class="left">
+          <div class="inner-wrapper">
+            <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          </div>
+        </div>
+        <div class="middle">
+          <div class="inner-wrapper">
+            <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper.</p>
+          </div>
+        </div>
+        <div class="right">
+          <div class="inner-wrapper">
+            <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div>
+        </div>
+      </div>
   </div>
 <?php include 'footer.php'; ?>
--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer class="clearfix">
   <div class="inner">
     <div class="left">
-      <span class="socket">Mama are mere</span>
+      <span class="socket">© Copyright <?php echo date("Y."); ?></span>
     </div>
     <div class="right">
-      <a class="social-icon" href="www.facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-      <a class="social-icon" href="www.facebook.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-      <a class="social-icon" href="www.facebook.com"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+      <a class="social-icon" href="http://facebook.com" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+      <a class="social-icon" href="http://instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+      <a class="social-icon" href="http://twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
     </div>
   </div>
 </footer>
