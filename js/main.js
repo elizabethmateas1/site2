c59a16039462fbd29c629e276871d360b36e55e3
Sergiu Goman <gomansergiu1@gmail.com>
2016-12-27 16:49:59 +0200

    added active class on menu item

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
 
-  var text_to_get = 'home';
+  // var text_to_get = 'home';
   var test_str = window.location.pathname;
   var start_pos = test_str.indexOf('/') + 1;
   var end_pos = test_str.indexOf('.',start_pos);
   var text_to_get = '.' + test_str.substring(start_pos,end_pos)
 
-  $(text_to_get).addClass('active');
-  console.log(text_to_get);
+  if (text_to_get == './') {
+    $('.home').addClass('active')
+  } else {
+    $(text_to_get).addClass('active');
+  }
 
 }); // end of document ready
